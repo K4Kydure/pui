299523cda4e5f84d724d0a0e21d33fc41cf2decf
NathanLithia <10991641+NathanLithia@users.noreply.github.com>
2026-05-30 20:22:54 +0930

    Update FixResourcePackOrder.bat
    This was generated by AI, run at your own risk

--- overrides/FixResourcePackOrder.bat
+++ overrides/FixResourcePackOrder.bat
@@ -1,15 +1,27 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
 
-set NEW=resourcePacks:["vanilla","mod_resources","Moonlight Mods Dynamic Assets","fabric","file/Mandala\u0027s GUI - Dark mode Mod Compat 0.2.7 beta Hotfix.zip",...]
+set "FILE=options.txt"
 
-(for /f "usebackq delims=" %%L in ("options.txt") do (
-    set line=%%L
+if not exist "%FILE%" (
+    echo options.txt not found.
+    exit /b 1
+)
+
+set "TMP=%TEMP%\options_%RANDOM%.tmp"
+
+(
+for /f "usebackq delims=" %%L in ("%FILE%") do (
+    set "line=%%L"
+
     if "!line:~0,14!"=="resourcePacks:" (
-        echo %NEW%
+        echo resourcePacks:["vanilla","mod_resources","file/Mandala\u0027s GUI - Dark mode Mod Compat 0.2.7 beta Hotfix.zip","file/Tetra_GUI_v.0.8.1.zip","file/Faithless 1.20.zip","file/Aether Regenerated v1.2.1.zip","file/Frozy\u0027s Quest Book.zip","file/Lopy-Create-Sophisticated-Backpacks.zip","file/Create Immersive Aircrafts Resource Pack 1.20.1 - 2.0.zip","file/Create Computers 1.2.1 - 1.20.1.zip","file/Create Simple Storage 2.1.zip","file/Better tom\u0027s create storage 1.1.zip","file/Createful-Plaques v0.6 (1.20.x).zip","file/Icon Xaero\u0027s 1.2 HF.zip","file/Enigmatic 3D (V1.6.0.2).zip","file/CU - 3d Wrench.zip","file/RoP v1.5.1.zip","file/Newer Age v0.2.zip","file/[Moonsu] Better GUI for Sophisticated Backpacks 1.0.zip","file/Alex\u0027s Titles.zip","file/Create_Addons_Fix_v1.zip","file/Visual Titles.zip","file/Suren Sophisticated Storage v1.0.1.zip","file/Faithless Fix.zip","file/Rename Compat Project.zip","file/2D Sprites - End Remastered 3D v.3.5 - 1.20.1.zip","Moonlight Mods Dynamic Assets","fabric","file/NathanLithia"]
     ) else (
-        echo %%L
+        echo(!line!
     )
-)) > options.tmp
+)
+) > "%TMP%"
 
-move /y options.tmp options.txt
+move /y "%TMP%" "%FILE%" >nul
+
+echo resourcePacks updated.
